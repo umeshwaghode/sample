3651fc715be842f664a76412bfad70bda37d054f
umeshwaghode <77615542+umeshwaghode@users.noreply.github.com>
2022-08-25 01:05:31 +0530

    Update PasswordViolation.java

--- PasswordViolation.java
+++ PasswordViolation.java
@@ -1,9 +1,9 @@
 public class PasswordViolation{
  
-  private String password = "hello world";
+  private String tt = "hello world";
   private String key = "0AB1CADE10ABDE01ABDDBEBE1010AA";
  
    public static void main(String args[]){
-      System.out.println("pasword " + password + args[5])
+      System.out.println("pasword " + tt + args[5])
    }
 }
